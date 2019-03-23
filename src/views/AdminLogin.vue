1ea426ebd721c911be52d4a7918602e60ea74947
haohaoangle <haohaoangle@163.com>
2019-03-23 23:29:59 +0800

    3.23

--- src/views/AdminLogin.vue
+++ src/views/AdminLogin.vue
@@ -17,7 +17,6 @@
       </el-form-item>
       <el-form-item prop="password">
         <span class="svg-container">
-          <svg-icon icon-class="mima" />
         </span>
         <el-input
           :type="passwordType"
@@ -27,7 +26,6 @@
           placeholder="密码"
           @keyup.enter.native="handleLogin"/>
         <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
         </span>
       </el-form-item>
       <el-form-item>
