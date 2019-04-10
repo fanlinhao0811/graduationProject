9057558f1955e78739c6e0538ffb2084aff6b719
haohaoangle <haohaoangle@163.com>
2019-04-10 08:32:08 +0800

    finder1

--- src/views/Finder.vue
+++ src/views/Finder.vue
@@ -2,7 +2,16 @@
 <template>
   <div class="finder">
     <div class="container">
-      <div>finder</div>
+      <div class="banner"></div>
+      <div class="item" v-for="(item,index) in list" :key="index">
+        <img :src="item.monent_img || `http://image.wufazhuce.com/FqOWy_yCMK1PGdYioZW8bN52UO96`" alt="">
+        <p>{{item.moment}}</p>
+        <p>{{item.id}}</p>
+        <div class="item-footer">
+          <div class="share">11</div>
+          <div class="like">22</div>
+        </div>
+      </div>
       <mfooter bgColor="rgb(121, 85, 72)"></mfooter>
     </div>
   </div>
@@ -11,7 +20,7 @@
 <script>
 import mfooter from '../components/Footer'
 import axios from 'axios'
-import toast from '../components/toast/index.js'
+// import toast from '../components/toast/index.js'
 export default {
   name: 'blog',
   data () {
@@ -25,115 +34,42 @@ export default {
   },
   created () {
     axios.get('/api/moment').then((res) => {
-      this.list = res.data
+      this.list = res.data.reverse()
       console.log(this.list)
     })
   },
   methods: {
-    compare (property) {
-      return function (obj1, obj2) {
-        var value1 = obj1[property]
-        var value2 = obj2[property]
-        return value2 - value1
-      }
-    },
-    login () {
-      console.log(this.xx)
-      toast('请输入账号密码！')
-      //   this.$store.state.isLogin = this.xx
-      this.$store.commit('login', this.xx)
-      document.cookie = 'isLogin=true'
-    //   console.log(this.$store.state.isLogin)
-    //   alert('请输入账号密码！')
-    },
-    setValue () {
-      axios.post('/api/setValue', {
-        name: this.xx
-      }).then((res) => {
-        console.log('res', res)
-      })
-    },
-    newUser () {
-      axios.post('/api/newUser', {
-        name: this.xx
-      }).then((res) => {
-        console.log('res', res)
-      })
-    }
   }
 }
 </script>
 <style lang="scss" scoped>
-    .login{
-        position: absolute;
-        top: 0;
-        left: 50%;
-        width: 100%;
-        max-width: 750px;
-        transform: translateX(-50%);
-        height: 100vh;
-        background: url("../assets/images/l-r-bg.jpeg") no-repeat;
-        background-size: 100% auto;
-        background-color: #fff;
-        .container{
-            width: 100%;
-            position: relative;
-            .input-email, .input-password{
-                width: 80%;
-                margin-left: 10%;
-                height: 50px;
-                border: 1px solid rgb(219,219,219);
-                padding: 20px 35px 20px 0;
-                display: flex;
-                align-items: center;
-                box-sizing: border-box;
-                .label{
-                    width: 30%;
-                    font-size: 24px;
-                    color: #999999;
-                }
-                input{
-                    background:none;
-                    outline:none;
-                    -webkit-appearance: none;
-                    border-radius: 0;
-                    width: 70%;
-                    height: 35px;
-                    border: 0;
-                    border-left: 1px solid rgb(219,219,219);
-                    padding-left: 20px;
-                }
-            }
-            .input-email{
-                margin-top: 80%;
-            }
-            .input-password{
-                margin-top: 10%;
-            }
-            .btn{
-                width: 50%;
-                margin: 10% 25% 0;
-                height: 60px;
-                border-radius: 10px;
-                background-color: rgb(219,219,219);
-                font-size: 24px;
-                color: #fff;
-                font-weight: bold;
-                line-height: 60px;
-                text-align: center;
-            }
-            .btn.active{
-                background-color: #666;
-            }
-            .tips{
-                width: 100%;
-                margin-top: 15%;
-                font-size: 22px;
-                color: #666666;
-                padding: 5px 0;
-                display: flex;
-                justify-content: space-around;
-            }
-        }
-    }
+.container {
+  font-size: 0.3rem;
+  width: 100%;
+  background: #ccc;
+  margin-bottom: 50px;
+}
+.container .item {
+  margin-top: 0.2rem;
+  background: #fff;
+}
+.container .item img {
+  display: block;
+  width: 100%;
+}
+.container .item p:nth-child(2) {
+  width: 80%;
+  margin-left: 10%;
+  line-height: 0.5rem;
+  text-align: left;
+}
+.container .item p:nth-child(3) {
+  margin-top: 0.8rem;
+}
+.item-footer {
+  width: 90%;
+  margin-left: 5%;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
